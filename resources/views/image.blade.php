67a0eb094f537ce10b6e8b1e52ec72eade58a8b0
Ahlul Aziz <ahlulazizap@gmail.com>
2023-05-23 22:01:21 +0700

    small changes

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -28,11 +28,11 @@
             </div>
             <div class="mt-3">
                 <label for="initVectorEncrypt" class="block leading-6 text-gray-900">Initialization Vector</label>
-                <input type="text" name="initVectorEncrypt" id="initVectorEncrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 Characters" required>
+                <input type="text" name="initVectorEncrypt" id="initVectorEncrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 characters" required>
             </div>
             <div class="mt-3">
                 <label for="encryptionKey" class="block leading-6 text-gray-900">Encryption Key</label>
-                <input type="text" name="encryptionKey" id="encryptionKey" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="abc" required>
+                <input type="text" name="encryptionKey" id="encryptionKey" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Any characters" required>
             </div>
             <p class="text-sm pt-2">Once you click the encrypt button below, the encrypted image should be downloaded to your personal computer automatically.</p>
             <div class="mt-5 text-center">
@@ -65,11 +65,11 @@
             </div>
             <div class="mt-3">
               <label for="initVectorDecrypt" class="block leading-6 text-gray-900">Initialization Vector</label>
-              <input type="text" name="initVectorDecrypt" id="initVectorDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 Characters" required>
+              <input type="text" name="initVectorDecrypt" id="initVectorDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 characters" required>
             </div>
             <div class="mt-3">
               <label for="encryptionKeyDecrypt" class="block leading-6 text-gray-900">Encryption Key</label>
-              <input type="text" name="encryptionKeyDecrypt" id="encryptionKeyDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="abc" required>
+              <input type="text" name="encryptionKeyDecrypt" id="encryptionKeyDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Any characters" required>
             </div>
             <p class="text-sm pt-2">Once you click the decrypt button below, the decrypted image should be downloaded to your personal computer automatically.</p>
             <div class="mt-5 text-center">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,11 +30,11 @@
             </div>
             <div class="mt-3">
               <label for="initVectorEncrypt" class="block leading-6 text-gray-900">Initialization Vector</label>
-              <input type="text" name="initVectorEncrypt" id="initVectorEncrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 Characters" required>
+              <input type="text" name="initVectorEncrypt" id="initVectorEncrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 characters" required>
             </div>
             <div class="mt-3">
               <label for="encryptionKey" class="block leading-6 text-gray-900">Encryption Key</label>
-              <input type="text" name="encryptionKey" id="encryptionKey" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="abc" required>
+              <input type="text" name="encryptionKey" id="encryptionKey" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Any characters" required>
             </div>
             <div class="mt-5 text-center">
               <button name="encrypt" type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500">Encrypt</button>
@@ -87,11 +87,11 @@
             </div>
             <div class="mt-3">
               <label for="initVectorDecrypt" class="block leading-6 text-gray-900">Initialization Vector</label>
-              <input type="text" name="initVectorDecrypt" id="initVectorDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 Characters" required>
+              <input type="text" name="initVectorDecrypt" id="initVectorDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Must be 16 characters" required>
             </div>
             <div class="mt-3">
               <label for="encryptionKeyDecrypt" class="block leading-6 text-gray-900">Encryption Key</label>
-              <input type="text" name="encryptionKeyDecrypt" id="encryptionKeyDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="abc" required>
+              <input type="text" name="encryptionKeyDecrypt" id="encryptionKeyDecrypt" class="mt-1 p-2.5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" placeholder="Any characters" required>
             </div>
             <div class="mt-5 text-center">
               <button name="decrypt" type="submit" class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500">Decrypt</button>
